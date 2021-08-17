f06c9fb84bb2f2e8bfb6131265ce3a0b7d546725
dev1 <dev1@mail.com>
2021-08-16 19:50:50 -0700

    modf by dev1

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
-calss hello{
-  public static void main()
+class hello()	#from dev1
+{
+  public static void main()	#from dev1
   {
-  	system.out.println("hello");
+  	system.out.println("hello from dev1")
   }
 }
